6a4029a57d073e5b7663025ce643bee7f681f31a
Emilien Puget <epuget@hi-media.com>
2015-07-17 12:06:25 +0200

    debug

--- trello-greasemonkey.user.js
+++ trello-greasemonkey.user.js
@@ -18,11 +18,11 @@ $(function () {
     });
 
     $('#content').bind('mousewheel DOMMouseScroll', function (event) {
-        if ($(event.target).parents('.list').length == 0) {
+        if ($(event.target).parents('.list').length === 0) {
             var $board = $('#board');
             if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
                 var next = $board.scrollLeft() - 280;
-                if ($board.scrollLeft() % 280 != 0) {
+                if ($board.scrollLeft() % 280 !== 0) {
                     next = Math.round($board.scrollLeft() / 280) * 280;
                 }
                 $board.scrollLeft(next);
